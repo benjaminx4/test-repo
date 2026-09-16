4f203e1b68b74ab2757363ba8ece3a6ba664c79d
Benjamin Xie <benjaminxieschool@gmail.com>
2026-09-16 17:25:46 -0400

    Division is scary

---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Division{
+  public static void main(String[] args){
+    System.out.println(5 / 3);
+    System.out.println(5.0 / 3);
+    System.out.println(5 / 3.0);
+    System.out.println(5.0 / 3.0);
+  }
+}
